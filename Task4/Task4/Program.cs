dd55674de5c7c988ad6b7be2657147476bb39b1e
Kanybek <mckanachangerofworld@gmail.com>
2019-01-26 12:23:42 +0600

    Task4

---
+++ Task4/Task4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n;
+            n = Convert.ToInt32(Console.ReadLine());
+            int[,] a = new int[n, n];
+
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = 0; j < n; j++)
+                {
+                    if (i >= j)
+                    {
+                        Console.Write("[*]");
+                    }
+                }Console.WriteLine();
+            }
+           
+        }
+    }
+}
